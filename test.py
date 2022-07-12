d24a46e3147879dd43ede621280b2243a2e65d68
vvfacc <vantoan591@gmail.com>
2022-07-12 16:00:29 -0600

    etst

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-for i in range(3):
+from bs4 import BeautifulSoup
+import requests
+
+HEADERS = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36"}
+
+for i in range(10):
     print(i)
